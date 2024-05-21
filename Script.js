18e74f8d0f9fe0a6b9ba25081179263c70a378ab
Simon <144794357+simonsartori@users.noreply.github.com>
2024-05-21 10:55:11 +0000

    -sdsd

--- Script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.side-menu');
-
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('side-menu.show');
-});

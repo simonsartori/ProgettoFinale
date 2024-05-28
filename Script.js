99f40aa58bccb35d26c898bf71f471013140c418
Simon <144794357+simonsartori@users.noreply.github.com>
2024-05-28 10:38:37 +0000

    stile home

---
+++ Script.js
@@ -0,0 +1,8 @@
+var divButton = document.querySelector('div.button-class');
+
+// Aggiungi un gestore di eventi al click del div
+divButton.addEventListener('click', function() {
+    // Azioni da eseguire quando il div viene cliccato
+    console.log('Il div è stato cliccato!');
+    // Puoi aggiungere qui la logica desiderata
+});
